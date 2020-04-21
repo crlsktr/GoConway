354267f51999c47143cda481966bf337a6dbd340
crlsktr <crlsktr@gmail.com>
2020-04-20 22:47:58 -0600

    function to generate matrix and read it

--- main.go
+++ main.go
@@ -1,21 +1,34 @@
 package main
 
+import (
+	"fmt"
+	"io/ioutil"
+	"os"
+)
+
 var size = 8
 
-func initializeBoard() [][]bool {
-	board := make([][]bool, size)
+func initializeBoard() [][]byte {
+	board := make([][]byte, size)
 	for i := 0; i < size; i++ {
-		board[i] = make([]bool, size)
+		board[i] = make([]byte, size)
 	}
 	return board
 }
 
 func main() {
+	// generateFile()
+	args := os.Args[1:]
 	board := initializeBoard()
-	// fmt.Println("before board:", board)
+	data,_ := ioutil.ReadFile(args[0])
+
 	for i := 0; i < size; i++ {
 		for j := 0; j < size; j++ {
-			board[i][j] = true
+			board[i][j] = data[(i*size)+j]
 		}
+	}
+
+	for _,row := range board {
+		fmt.Println(row,",")
 	}
 }
